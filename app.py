9fd37cf4a76bf0ed8dc56680d587b00909c782f1
Chuma Kabaghe <chumakabaghe@gmail.com>
2023-04-20 00:35:38 +0000

    add button and fix logic

--- app.py
+++ app.py
@@ -21,10 +21,14 @@ st.title("Card Genie")
 occasion = st.text_input("What is the occasion?", placeholder="birthday", max_chars=50)
 recipient = st.text_input("Who is the card for?", placeholder="neighbor", max_chars=50)
 tone = st.text_input("What tone would you like the card to take?", value="friendly", max_chars=120)
-personal_message = st.text_input("Add a personal detail if you'd like it to be incorporated in the card message", placeholder="thanks for helping us move in!", max_chars=100)
-if occasion and recipient is not None:
+personal_message = st.text_input("Add a personal detail if you'd like it to be incorporated in the card message",
+ placeholder="thanks for helping us move in!", max_chars=100)
+
+if st.button("Generate") and occasion != "" and recipient !="":
     with st.spinner("Generating message..."):
-        message_prompt = f"You're a professional card writer. Write a {occasion} card for my {recipient}. The card should be {tone}. Don't include any salutations or sign offs. In cases where gender is ambiguous, use gender neutral terminology."
+        message_prompt = f"You're a professional card writer. Write a {occasion} card for my {recipient}.\
+            The card should be {tone}. Don't include any salutations or sign offs. In cases where gender is\
+                 ambiguous, use gender neutral terminology."
         if len(personal_message) > 0:
             message_prompt += " Incorporate the following information to make it more personal: {personal_message}.",
-        generate_message(message_prompt)
+        
