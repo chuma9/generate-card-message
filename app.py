0f342f1613cd0f1b65053c89cc29af95666e1a14
Chuma Kabaghe <chumakabaghe@gmail.com>
2023-04-20 00:58:10 +0000

    fix indentation

--- app.py
+++ app.py
@@ -16,25 +16,25 @@ def generate_message(prompt: str):
         presence_penalty=0
     )
     message = response.choices and response.choices[0] and response.choices[0].text.strip();
-    st.markdown(message)
+    with col2:
+        st.markdown(message)
 
 st.title("Card Genie")
 col1, col2 = st.columns(2)
 
 with col1:
-occasion = st.text_input("What is the occasion?", placeholder="birthday", max_chars=50)
-recipient = st.text_input("Who is the card for?", placeholder="neighbor", max_chars=50)
-tone = st.text_input("What tone would you like the card to take?", value="friendly", max_chars=120)
-personal_message = st.text_input("Add a personal detail if you'd like it to be incorporated in the card message", 
-    placeholder="thanks for helping us move in!", max_chars=100)
+    occasion = st.text_input("What is the occasion?", placeholder="birthday", max_chars=50)
+    recipient = st.text_input("Who is the card for?", placeholder="neighbor", max_chars=50)
+    tone = st.text_input("What tone would you like the card to take?", value="friendly", max_chars=120)
+    personal_message = st.text_input("Add a personal detail if you'd like it to be incorporated in the card message", 
+        placeholder="thanks for helping us move in!", max_chars=100)
 
-if st.button("Generate") and occasion != "" and recipient !="":
-    with st.spinner("Generating message..."):
-        message_prompt = f"You're a professional card writer. Write a {occasion} card for my {recipient}.\
-            The card should be {tone}. Don't include any salutations or sign offs. In cases where gender is\
-                 ambiguous, use gender neutral terminology."
-        if personal_message != "":
-            message_prompt += f" Incorporate the following information to make it more personal: {personal_message}."
-        with col2:
-            generate_message(message_prompt)
+    if st.button("Generate") and occasion != "" and recipient !="":
+        with st.spinner("Generating message..."):
+            message_prompt = f"You're a professional card writer. Write a {occasion} card for my {recipient}.\
+                The card should be {tone}. Don't include any salutations or sign offs. In cases where gender is\
+                    ambiguous, use gender neutral terminology."
+            if personal_message != "":
+                message_prompt += f" Incorporate the following information to make it more personal: {personal_message}."
+                generate_message(message_prompt)
         
